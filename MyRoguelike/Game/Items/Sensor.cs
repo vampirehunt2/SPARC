1b6ef6fe1087155542c2864f68d2bc870267a6c1
vampirehunt2 <70143882+vampirehunt2@users.noreply.github.com>
2024-04-03 08:28:13 +0200

    made items activable

---
+++ MyRoguelike/Game/Items/Sensor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VH.Engine.World.Items;
+
+namespace SPARC.Game.Items {
+    public class Sensor : Item, IActivable {
+
+        private bool active;
+        public Sensor() { }
+        public bool Active {
+            get => active; 
+            set => active = value; 
+        }
+    }
+}
---
+++ MyRoguelike/Game/Items/SparcMissleWeapon.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VH.Engine.World.Items.Weapons;
+
+namespace SPARC.Game.Items {
+    public class SparcMissleWeapon : MissleWeapon, IActivable {
+
+        private bool active;
+
+        public SparcMissleWeapon() { }
+        public bool Active {
+            get => active;
+            set => active = value;
+        }
+    }
+}
